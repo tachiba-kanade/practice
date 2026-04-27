42fdff4c2625c048fc5d334e7ca37b5bcf876c49
tachiba-kanade <work.sudhas@gmail.com>
2026-04-27 14:56:41 +0530

    just practicing basic

---
+++ telusko/loop.py
@@ -0,0 +1,12 @@
+"""Loops for Everyone"""
+
+#1. While Loop - it's conditional
+i = 0
+while i < 5:
+    print(i)
+    i=i+1
+
+#2. For Loop - it's Iterative for sequences for list, tuple, string
+
+for i in range(11, 20, 2):
+    print(i)
---
+++ telusko/normal.py
@@ -0,0 +1,10 @@
+# x = input("Enter 1st number: ")
+# y = input("Enter 2nd number: ")
+
+# z = int(x) + int(y)
+# print(z)
+
+import sys
+ch = input("Enter a character: ")[0]
+print("result: " + ch)
+
