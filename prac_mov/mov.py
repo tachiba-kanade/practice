55dc389c3c376c5fec0a113d8ca3bbda20b319d3
sudha19 <work.sudhas@gmail.com>
2026-06-20 22:18:29 +0530

    practice

---
+++ prac_mov/mov.py
@@ -0,0 +1,25 @@
+"""
+GOAL 1
+
+- user search movies
+- user add movie/save to local 
+- user can view the saved
+
+Data layer: Trakt API, saved movies, ratings, movie details
+Logic layer: add movie, mark watched, calculate stats
+
+First make search_movies(query)
+Then make save_movie(movie)
+Then make mark_watched(movie_id)
+
+movie-tracker
+→ search movie
+→ add to watchlist
+→ view watchlist
+→ mark watched
+→ rate movie
+→ view watched history
+
+"""
+
+
