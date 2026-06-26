8cc2999e3fab06204bf6d78205bbe84409adfc73
sudha19 <work.sudhas@gmail.com>
2026-06-26 19:12:58 +0530

    cpin stim

--- beginners_qs/coin_flip_stimulator.py
+++ beginners_qs/coin_flip_stimulator.py
@@ -5,10 +5,33 @@ How do we represent a coin?
 
 """
 import numpy as np
-#Instead of one flip, generate 10.
-flip = np.random.choice([0,1],10)
-print(flip)
+from scipy.stats import norm
+#Instead of one flip, generate 10.\
 
-heads = np.sum(flip)
+res = []
+for i in range (5000):
+    flip = np.random.choice([0,1],10000)
+    print(flip)
+    heads = np.sum(flip)
+    print(heads)
+    res =np.append(heads)
 
-print(heads)
+print(res)
+
+probabilty  = np.mean(np.array(res)>5200)
+
+import matplotlib.pyplot as plt
+plt.hist(res)
+plt.show()
+
+
+"""
+Probability theory
+Python programming
+Simulation (Monte Carlo)
+Statistical inference (Central Limit Theorem)
+
+"""
+
+
+
